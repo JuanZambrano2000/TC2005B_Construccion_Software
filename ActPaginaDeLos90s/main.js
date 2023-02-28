114de1d3404f360bd38784f449f28f09c9bad563
Juan Zambrano <jpablozambrano@hotmail.com>
2023-02-27 22:19:33 -0600

    mini_reto_2

--- ActPaginaDeLos90s/main.js
+++ ActPaginaDeLos90s/main.js
@@ -1,4 +1,4 @@
-//
+
 function changeColor(newColor){
     var elem = document.getElementById('titulo1');
     elem.style.color = newColor;
@@ -8,3 +8,16 @@ function changeSize(size){
     var elem = document.getElementById('botoncito');
     elem.style.fontSize = size;
 }
+
+function cargarImagen() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      const objetoUrl = URL.createObjectURL(xhttp.response);
+      const imgElement = document.getElementById("mi-imagen");
+      imgElement.src = objetoUrl;
+      
+    };
+    xhttp.open("GET", "gatito.jpeg");
+    xhttp.responseType = "blob";
+    xhttp.send();
+  }
