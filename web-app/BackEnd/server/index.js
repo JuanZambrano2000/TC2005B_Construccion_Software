5340cd0529422ab463f70966aa6427d5fc9cb4f5
Juan Zambrano <jpablozambrano@hotmail.com>
2023-03-28 10:06:24 -0600

    Practica React
    Conexion back-front end

--- web-app/BackEnd/server/index.js
+++ web-app/BackEnd/server/index.js
@@ -1,31 +1,28 @@
+// server/index.js
 const express = require("express");
+const bodyParser = require('body-parser');
 const fs = require("fs");
-const bodyParser = require("body-parser");
-const { data } = require("jquery");
+
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
-app.get("/api", (req,res) =>{
-    res.json({message: "Hello from server side"});
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from server side!"});
 });
 
-app.get("/api/pet",(req, res)=>{
-    fs.readFile(__dirname + "/pets.json","utf-8",(err,data)=>{
-        console.log(data);
-        res.end(data);
-    });
+app.get("/api/pet", (req, res) => {
+  fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
+    console.log( data );
+    res.end( data );
+  });
 });
 
-app.post("/api/pet",(req, res)=> {
-    console.log("El cuerpo y vida de la peticion: ", req.body);
-    res.statusCode(200);
-})
-
-app.listen(PORT, () =>{
-    console.log(`Server listening on ${PORT}`);
+app.post("/api/pet", (req, res) => {
+  console.log('El cuerpo de la peticion:', req.body);
 });
 
-
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
