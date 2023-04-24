14df8516bacb351206c988b70af32ded3c44a6f5
Juan Zambrano <jpablozambrano@hotmail.com>
2023-04-24 09:05:05 -0600

    Update index.js

--- web-app/BackEnd/server/index.js
+++ web-app/BackEnd/server/index.js
@@ -2,14 +2,37 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const fs = require("fs");
-
-
+const mysql = require("mysql");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'apiuser',
+  password: '1234',
+  database: 'empresa'
+})
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+app.get("/api/table", (req, res)=>{
+  connection.query('SELECT * FROM EMPLOYEE', (err, rows) => {
+    if (err) throw err
+    res.send(rows);
+  })
+  connection.end();
+});
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from server side!", dogsName: "Buddy"});
+  connection.end();
 });
 
 app.get("/api/pet", (req, res) => {
@@ -17,12 +40,14 @@ app.get("/api/pet", (req, res) => {
     console.log( data );
     res.end( data );
   });
+  connection.end();
 });
 
 app.post("/api/pet", (req, res) => {
   console.log('El cuerpo de la peticion:', req.body);
+  connection.end();
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
-});
+})
